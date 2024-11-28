cfaf1ce5a3f907cc2a97c525addb37e11ac6da89
Albert Qi <albertqi03@gmail.com>
2024-11-28 10:27:52 -0600

    Add process.py

---
+++ prebuilt/README.md
@@ -0,0 +1,5 @@
+# Prebuilt 'scsynth' File
+
+The `scsynth` file is an audio server provided by SuperCollider that is used for audio analysis, synthesis, and processing. More information can be found [here](https://github.com/supercollider/supercollider).
+
+MiniClef uses a prebuilt version of `scsynth` so that users do not need to install SuperCollider itself. The specific prebuilt version of `scsynth` is taken from [Sonic Pi](https://github.com/sonic-pi-net/sonic-pi).
---
+++ scd/process.py
@@ -0,0 +1,24 @@
+import os
+
+
+def main() -> None:
+    """Consolidate all SuperCollider code in the 'effects' and 'synths' folders into a single file."""
+
+    with open("out.scd", "w") as out:
+        out.write("(\n")
+        for dir in ("effects", "synths"):
+            files = os.listdir(dir)
+            for file in files:
+                with open(f"{dir}/{file}") as f:
+                    # Replace 'add' with 'writeDefFile'.
+                    txt = f.read()
+                    replace_txt = f'writeDefFile("{os.getcwd()}/compiled")'
+                    new_txt = txt[::-1].replace("add"[::-1], replace_txt[::-1], 1)[::-1]
+                    out.write(new_txt + "\n")
+        out.write(")\n")
+
+    print("Done!")
+
+
+if __name__ == "__main__":
+    main()
